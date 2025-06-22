9e0786962e201b1ccb6a0053c8ff0a451d1e1d4d
Kira-NT <kira.canary@proton.me>
2025-06-22 20:32:33 +0100

    Enable VPN in `vpn-toggle.sh` only while alert is active
    This behavior works much better with Steam, for example, which doesn't handle programs that leave loose background processes very well.

--- scripts/vpn-toggle.sh
+++ scripts/vpn-toggle.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-zenity --info --title "Outline" --text "$(pkexec /usr/local/bin/__vpn_manager toggle)" --width 300 2> /dev/null
+zenity --info --title "Outline" --text "$(pkexec /usr/local/bin/__vpn_manager connect)" --width 300 2> /dev/null
+pkexec /usr/local/bin/__vpn_manager disconnect > /dev/null 2>& 1
